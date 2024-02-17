d10ccdacd2d00c0413cbafb3ca15d873323c913d
Alena <alena.shestakova.92@inbox.ru>
2024-02-17 15:46:04 +0700

    функции 1.4

--- sandbox/src/main/java/Geometry.java
+++ sandbox/src/main/java/Geometry.java
@@ -1,12 +1,25 @@
 public class Geometry {
     public static void main(String[] args) {
-        var side=5.;
-        System.out.println("площадь квадрата со стороной "+ side +" = "+(side*side));
-        side=5.0;
-        side=5;
+        printSquareArea(7);
 
-        var a="2";
-        a="3";
-        a=""+3;
+        printRwectangleArea(3.0, 5.0);
+printRwectangleArea(7.,9.);
+    }
+
+    private static void printRwectangleArea(double a, double b) {
+        System.out.println("площадь квадрата со сторонами "+ a +" и "+ b+" = "+ rectangleArea(a,b));
+
+    }
+
+    private static double rectangleArea(double a, double b) {
+        return a*b;
+    }
+
+    static void printSquareArea(double side) {
+        System.out.println("площадь квадрата со стороной "+ side +" = "+ squareArea(side));
+    }
+
+    private static double squareArea(double a) {
+        return a * a;
     }
 }
