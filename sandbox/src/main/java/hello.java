9ed0cdf156bc88df42e576469cb89dabdcde7781
Alena <alena.shestakova.92@inbox.ru>
2024-02-17 14:43:57 +0700

    dshf;tybz

--- sandbox/src/main/java/hello.java
+++ sandbox/src/main/java/hello.java
@@ -1,6 +1,8 @@
-public class hello
-{
+public class hello {
     public static void main(String[] args) {
         System.out.println("hello ");
+        System.out.println(2+2);
+        System.out.println(2/2);
+        System.out.println(2+2*2);
     }
 }
