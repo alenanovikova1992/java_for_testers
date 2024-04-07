6145dc3683e452468acaa7e5ee9cebac36cf3f1f
Alena <alena.shestakova.92@inbox.ru>
2024-04-07 18:10:32 +0700

    Задание 17 Не получилось обратиться к файлу, прописала явно.

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -36,9 +36,13 @@ public class ApplicationManager {
             else {
                 throw  new IllegalArgumentException(String.format("Unknown browser %s", string));}
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get(properties.getProperty("web.baseUrl"));
+          //  driver.get(properties.getProperty("web.baseUrl"));
+            driver.get("http://localhost/mantisbt-2.26.1/mantisbt-2.26.1/");
             driver.manage().window().setSize(new Dimension(1302, 732));
-             session().login (properties.getProperty("web.username"),properties.getProperty("web.password"));
+          //   session().login (properties.getProperty("web.username"),properties.getProperty("web.password"));
+          //  driver.manage().window().setSize(new Dimension(1302, 732));
+            // driver.get("http://localhost/mantisbt-2.26.1/mantisbt-2.26.1/");
+            session().login ("administrator", "root");
         }
         return driver;
     }
--- mantis_tests/src/test/java/ru/stqa/mantis/manager/HelperBase.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/HelperBase.java
@@ -1,28 +1,18 @@
 package ru.stqa.mantis.manager;
-
 import org.openqa.selenium.By;
-
-import java.nio.file.Paths;
-
 public class HelperBase {
     protected final ApplicationManager manager;
-
     public HelperBase(ApplicationManager manager) {
         this.manager = manager;
     }
-
     protected void type(By locator, String text) {
         click(locator);
         manager.driver().findElement(locator).clear();
         manager.driver().findElement(locator).sendKeys(text);
     }
-    protected void attach(By locator, String file) {
-        manager.driver().findElement(locator).sendKeys(Paths.get(file).toAbsolutePath().toString());
-    }
 protected boolean isElementPresent(By locator){
      return manager.driver().findElements(locator).size()>0;
 }
-
     protected void click(By locator) {
         manager.driver().findElement(locator).click();
     }
