eade889b96bb66a87a65f9b96c0f7b772941df3d
Alena <alena.shestakova.92@inbox.ru>
2024-04-19 23:22:20 +0700

    Задание 20

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -3,8 +3,14 @@ package ru.stqa.mantis.manager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 public class ApplicationManager {
@@ -25,14 +31,26 @@ public class ApplicationManager {
         return sessionHelper;
     }
 
-    public    WebDriver driver() {
+    public    WebDriver driver() throws MalformedURLException {
         if (driver == null){
+            var seleniumServer  = properties.getProperty("SeleniumServer");
             if ("Google Chrome".equals(string)){
-                driver = new ChromeDriver();
+                if (seleniumServer != null){
+                    driver  = new RemoteWebDriver(new URL(seleniumServer), new ChromeOptions());
+                } else
+                {
+                    driver = new ChromeDriver();
+                }
+
+
             }
             else if ("Microsoft Edge".equals(string))
+            {if (seleniumServer != null)
             {
-                driver= new EdgeDriver();}
+                driver  = new RemoteWebDriver(new URL(seleniumServer), new EdgeOptions());
+            }
+             else {
+                driver= new EdgeDriver();}}
             else {
                 throw  new IllegalArgumentException(String.format("Unknown browser %s", string));}
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
